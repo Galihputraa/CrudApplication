f904e0ef20d4b008c31a251b113f2d20214533cb
GalihJaya <galihputraa11@github.com>
2017-12-07 03:08:49 +0700

    Initial commit

---
+++ app/src/main/java/com/galihjayaningrat/crudapplication/DataHelper.java
@@ -0,0 +1,29 @@
+package com.galihjayaningrat.crudapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by GALIH on 06 Des 2017.
+ */
+
+public class DataHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "biodatadiri.db";
+    private static final int DATABASE_VERTSION = 1;
+    public DataHelper(Context context){
+        super(context, DATABASE_NAME,null,DATABASE_VERTSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        String sql = "create table biodata(no integer primary key, nama text null, tgl text null, jk text null, alamat text null);";
+        Log.d("Data","onCreate: "+sql);
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2){
+
+    }
+}
